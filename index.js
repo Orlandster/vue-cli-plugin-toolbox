014aed8506c0b753f578da32ca30033065bd5a1a
Orlando Wenzinger <orlando.wenzinger@bluewin.ch>
2019-05-31 21:27:37 +0200

    [Feature]: Create dynamic component generator

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 const { createFile } = require('./lib/fs');
 const { renderFile } = require('./lib/ejs');
+const { getArguments, getFormattedNames } = require('./lib/utils');
 
-module.exports = (api, opts) => {
+module.exports = (api) => {
     api.registerCommand(
         'generate',
         {
           description: 'Generates Components, Directive and Filters.',
-          usage: 'npx vue-cli-service generate'
+          usage: 'npx vue-cli-service generate',
         },
         async () => {
           const projectRoot = process.cwd();
-
-          console.log(projectRoot)
-
-          let component = {
-            name: 'New Component',
-            nameSnakeCase: 'new-component',
-            nameCamelCase: 'newComponent',
-            nameUpperCamelCase: 'NewComponent',
-          };
-
-          const renderedFile = await renderFile(`${__dirname}/templates/components/single-file/Template.vue`, component);
-          await createFile(`${projectRoot}/src/components/Name.vue`, renderedFile)
+          const nameArg = getArguments()[1];
+          const formattedNames = getFormattedNames(nameArg);
+          const renderedFile = await renderFile(`${__dirname}/templates/components/single-file/Template.vue`, formattedNames);
+          await createFile(`${projectRoot}/src/components/${formattedNames.nameUpperCamelCase}.vue`, renderedFile)
         }
     )
 }
