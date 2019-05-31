b4349371e667b8f8e446d64406ad1190d821ca13
Orlando Wenzinger <orlando.wenzinger@bluewin.ch>
2019-05-31 21:43:55 +0200

    [Feature]: Add error and success handler

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { createFile } = require('./lib/fs');
 const { renderFile } = require('./lib/ejs');
-const { getArguments, getFormattedNames } = require('./lib/utils');
+const { getArguments, getFormattedNames, logError, logSuccess } = require('./lib/utils');
 
 module.exports = (api) => {
     api.registerCommand(
@@ -14,7 +14,14 @@ module.exports = (api) => {
           const nameArg = getArguments()[1];
           const formattedNames = getFormattedNames(nameArg);
           const renderedFile = await renderFile(`${__dirname}/templates/components/single-file/Template.vue`, formattedNames);
-          await createFile(`${projectRoot}/src/components/${formattedNames.nameUpperCamelCase}.vue`, renderedFile)
+          const filePath = `${projectRoot}/src/components/${formattedNames.nameUpperCamelCase}.vue`;
+
+          try {
+            await createFile(filePath, renderedFile)
+            logSuccess(`Successfully generated the Component! \n${filePath}`);
+          } catch (e) {
+            logError(e);
+          }
         }
     )
 }
